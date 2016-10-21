fcd8936f98351d428c858c4d2849c1ba2d4171e0
Pawel Galazka <pawel.galazka1986@gmail.com>
2016-10-22 00:50:51 +1100

    Tests for CoreLogger

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,73 @@
-/* eslint-env mocha */
-describe('test', () => {
-  it('test something', () => {
+/* eslint-env jest */
+import { CoreLogger } from '../lib/index'
 
+describe('CoreLogger', () => {
+  describe('without any middleware', () => {
+    let logger
+
+    beforeEach(() => {
+      logger = new CoreLogger()
+    })
+
+    it('should do nothing', () => {
+      logger.log(10, 'test message')
+    })
+
+    it('should have no middlewares', () => {
+      expect(logger.middlewares).toEqual([])
+    })
+
+    it('should be immutable', () => {
+      expect(() => {
+        logger.middlewares.push(() => {})
+      }).toThrowError(/object is not extensible/)
+      logger.use(() => {})
+      expect(logger.middlewares).toEqual([])
+    })
+  })
+
+  describe('with middleware', () => {
+    it('should be able to filter out the logs', () => {
+      let calls = []
+      const logger = new CoreLogger()
+        .use((next, level, message, extras) => {
+          calls.push([level, message, extras])
+          next(message, extras)
+        })
+        .use((next, level, message, extras) => {
+          calls.push([level, message, extras])
+          next(message, extras)
+        })
+        .use((next, level, message, extras) => {
+          calls.push([level, message, extras])
+        })
+
+      logger.log(10, 'test msg', 'extras')
+      expect(calls).toEqual([
+        [10, 'test msg', 'extras'],
+        [10, 'test msg', 'extras']
+      ])
+    })
+
+    it('should be able to format the logs', () => {
+      let calls = []
+      const logger = new CoreLogger()
+        .use((next, level, message, extras) => {
+          message = '[note] ' + message
+          calls.push([level, message, extras])
+          next(message, extras)
+        })
+        .use((next, level, message, extras) => {
+          message = message + ' [suffix]'
+          calls.push([level, message, extras])
+          next(message, extras)
+        })
+
+      logger.log(10, 'test msg', 'extras')
+      expect(calls).toEqual([
+        [10, '[note] test msg', 'extras'],
+        [10, '[note] test msg [suffix]', 'extras']
+      ])
+    })
   })
 })
---
+++ src/middleware.js
@@ -0,0 +1,7 @@
+export function levelFilter (next, level, message, extras) {
+
+}
+
+export function consoleStream (next, level, message, extras) {
+
+}
